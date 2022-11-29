fcbd623cf0a0faa492d1f670eb692a0108e4e9a3
Mj7890 <61232315+Mj7890@users.noreply.github.com>
2022-11-29 15:53:26 +0530

    Add files via upload

---
+++ E-commerce.js
@@ -0,0 +1,27 @@
+var MenuLists = document.getElementById("MenuItems");
+MenuLists.style.display="0px";
+function menutoggle(){
+    if(MenuLists.style.maxHeight=="0px"){
+       MenuLists.style.maxHeight="200px"; 
+    }
+    else{
+        MenuLists.style.maxHeight="0px";
+    }
+     
+}
+// Js for product gallery
+var ProductImg = document.getElementById("ProductImg");
+var SmallImg = document.getElementByClassName("small-img")
+SmallImg[0].onclick = function(){
+    ProductImg.src =SmallImg[0].src;
+}
+SmallImg[1].onclick = function(){
+    ProductImg.src =SmallImg[1].src;
+}
+SmallImg[2].onclick = function(){
+    ProductImg.src =SmallImg[2].src;
+}
+SmallImg[3].onclick = function(){
+    ProductImg.src =SmallImg[3].src;
+}
+
